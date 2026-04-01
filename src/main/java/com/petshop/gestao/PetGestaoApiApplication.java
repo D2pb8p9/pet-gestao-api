297c645b35275c09e5f3a8ced5500576189340fd
Daniel <daniel.pires@ciandt.com>
2026-04-01 10:43:13 -0300

    Initial commit

---
+++ src/main/java/com/petshop/gestao/PetGestaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.petshop.gestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetGestaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetGestaoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petshop/pet_gestao_api/PetGestaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petshop.pet_gestao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetGestaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
