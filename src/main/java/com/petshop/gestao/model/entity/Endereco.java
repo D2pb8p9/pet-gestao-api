e1d6fa17b6f2dc8dcf401b0c52c574d8a905829b
Daniel <daniel.pires@ciandt.com>
2026-04-01 17:04:52 -0300

    fix: corrige e acrescenta validações e anotações do Spring, e enum para tipoPelagem

--- src/main/java/com/petshop/gestao/model/entity/Endereco.java
+++ src/main/java/com/petshop/gestao/model/entity/Endereco.java
@@ -1,10 +1,15 @@
-package com.petshop.gestao.model;
+package com.petshop.gestao.model.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -23,18 +28,36 @@ import lombok.Setter;
 public class Endereco {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
+    @NotBlank
+    @Column(nullable = false)
     private String logradouro;
 
+    @NotBlank
+    @Column(nullable = false)
     private String numero;
 
     private String complemento;
 
+    @NotBlank
+    @Column(nullable = false)
     private String bairro;
 
+    @NotBlank
+    @Column(nullable = false)
     private String cidade;
 
+    @NotBlank
+    @Pattern(regexp = "\\d{5}-\\d{3}")
+    @Column(nullable = false)
     private String cep;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String estado;
+
+    @Version
+    private Long version;
 }
